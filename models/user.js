aba5c284d00b7f041d36ebce564a83efa26b4e4e
bs317482 <bernardbnard@gmail.com>
2018-04-12 19:48:28 -0400

    testing17

--- models/user.js
+++ models/user.js
@@ -1,4 +1,18 @@
 var mongoose = require('mongoose')
 var Schema = require('../db/schema')
 var User = mongoose.model('User',Schema.UserSchema)
+
+
+
+const UserSchema = new Schema({
+    fullName: String,
+    food: String,
+    gym: String,
+    weight: String,
+    email: String,
+    })
+  
+
+
+
 module.exports = User
