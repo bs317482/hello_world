e3a14ddc6d7d40d2c886e6c690e9a1c10906bedb
bs317482 <bernardbnard@gmail.com>
2018-04-16 11:46:13 -0400

    testing20

---
+++ db/seeds.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+
+const User = require('../models/User');
+const Food = require('../models/Food');
+const Gym = require('../models/Gym');
+const db = mongoose.connection
+
+mongoose.connect(process.env.MONGODB_URI);
+
+
+db.on('open', () => {
+    console.log('buttche')
+})
+db.on('error', (error) => {
+    console.log(error)
+})
+
+const bleu = new User({
+    name: 'bleu',
+})
+        
+User.remove().then(() => {
+    return User.insertMany([bleu])
+}).then((users) => {
+    console.log(users)
+    console.log('Saved User')
+    db.close()
+}).catch((error) => {
+    console.log(error)
+    db.close()
+})
